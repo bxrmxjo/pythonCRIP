4a1ef8c38a7e120dcbb39944c662958989388e39
bxrmxjo <76953086+bxrmxjo@users.noreply.github.com>
2024-10-27 20:09:18 +0100

    Update ejemPock.py

--- ejemPock.py
+++ ejemPock.py
@@ -25,3 +25,11 @@ print("\n","N = ",N,"\n","Nº dígitos de N: ",len(str(N)),"\n","fact = ",fact,"
 N = pow(47, 1039)*pow(7,284)*random.randint(1,pow(47,1039))
 fact = [7,47]
 print("\n","N = ",N,"\n","Nº dígitos de N: ",len(str(N)),"\n","fact = ",fact,"\n", "¿Es N primo?: ", po.pocklington_lehmer_generalizado(fact,N))
+
+#Verificación de primalidad de algunos primos de Proth
+ENE = [449,3137,11777,133121,219649,3452929,15998977]
+for t in ENE:
+    inicio = time.time()
+    res = pocklington_lehmer_generalizado([2],t)
+    fin = time.time()
+    print(f"{t} verifica ser primo: {res}.    TT: {(fin - inicio):.6f}s")
